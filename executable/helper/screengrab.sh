48b4945bdcb3da8acdc7a903dd7de36ec220fe98
S M <sayhar@gmail.com>
2014-02-10 14:23:30 -0800

    screengrab is a bit more verbose

--- executable/helper/screengrab.sh
+++ executable/helper/screengrab.sh
@@ -4,12 +4,14 @@
 [ -z $1 ] && echo "need a banner"
 [ -z $1 ] && exit
 
-URL=$(echo "http://en.wikipedia.org/wiki/Declaration_of_Rights_of_Man?force=1&banner=$1") &&
-#echo $URL &&
+URL=$(echo "http://en.wikipedia.org/wiki/Declaration_of_Rights_of_Man?force=1&reset=1&banner=$1") &&
+echo $URL &&
 cd ../ &&
 cd ../output &&
+echo "webkitting" &&
 webkit2png --delay=1 -F -o banner -W 1400 -H 800 $URL > /dev/null &&
 mv banner-full.png banner.png &&
+echo "converting"
 convert banner.png -crop 1400x170 thumb.png > /dev/null &&
 mv thumb-0.png $1.png &&
 rm thumb* &&
@@ -17,14 +19,10 @@ convert banner.png -crop 1400x500 thumb.png > /dev/null &&
 mv thumb-0.png $1-full.png &&
 rm thumb* &&
 rm banner.png &&
+echo "converted. now s3cmding" &&
 s3cmd del s3://wikitoy/screenshots/$1.png > /dev/null
 s3cmd del s3://wikitoy/screenshots/$1-full.png > /dev/null
 s3cmd put $1.png s3://wikitoy/screenshots/$1.png --acl-public > /dev/null &&
 s3cmd put $1-full.png s3://wikitoy/screenshots/$1-full.png --acl-public > /dev/null &&
 S3URL=http://wikitoy.s3.amazonaws.com/screenshots/$1.png  > /dev/null &&
 echo $S3URL
-
-cd ../executable &&
-cd ./helper
-
-
