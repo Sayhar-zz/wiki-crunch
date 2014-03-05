6eda3ea61574c39d5f322a5fcebaeae36bdc2664
S M <sayhar@gmail.com>
2014-03-04 16:06:57 -0800

    Remove dependency on googlecl

--- executable/helper/old_doc_downloader.py
+++ executable/helper/old_doc_downloader.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python
 #doc_downloader.py
 #needs gspread package
-
+#WARNING - doesn't work due to unicode. Sorry!
+import pdb
 import csv
 import gspread
 import json
+import codecs
+import cStringIO
 
 config = json.load(open('config.json'))
 email = config['email']
@@ -12,12 +15,22 @@ password = config['password']
 gc = gspread.login(email, password)
 
 threeFiles = {"testForm.tsv":"0ArMJ6Soyh6dxdGllR0k2ZTVNN0kwZkltbk96QTQxLVE", "TLBVVCL.tsv":"0ArMJ6Soyh6dxdDAtZ0pSZ1UyNzB4OVAwbkJvRnlwVlE","screenshots.tsv":"0ArMJ6Soyh6dxdDhCZzB3ZXFKZ3FUdThzenp0U0pUUnc"}
+#threeFiles = {"testForm.tsv":"0ArMJ6Soyh6dxdGllR0k2ZTVNN0kwZkltbk96QTQxLVE", "screenshots.tsv":"0ArMJ6Soyh6dxdDhCZzB3ZXFKZ3FUdThzenp0U0pUUnc"}
+
+
 
 for f in threeFiles:
+#	print f
 	filename = "../../data/" + f
 	sh = gc.open_by_key(threeFiles[f])
 	ws = sh.get_worksheet(0)
-	with open(filename, 'wb') as ff:
-		writer = csv.writer(ff, dialect="excel-tab")
-		writer.writerows(ws.get_all_values())
-		print "written to " + filename
+	ff = codecs.open(filename, mode='wb', encoding='utf-8', errors="replace")
+	writer = csv.writer(ff, dialect="excel-tab")
+	
+	rows = ws.get_all_values()
+	for row in rows:
+		try:
+			writer.writerow(row)
+		except:
+			print "\tERROR! Test " + row[12] + " did not download! (Probably because unicode)" 
+	print "written to " + filename
